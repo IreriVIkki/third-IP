be37bf6b5d846f8f3b898229510873615df143c7
IreriVikki <wambsviki@gmail.com>
2018-07-13 12:16:50 +0300

    adding spans for ping result

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,12 @@
 $(document).ready(function(){
+
     //  business logic
 
     var input = 34
 
     function playPingPong(num){
         if((num % 3 === 0) && (num % 5 !== 0)){
+            $('#results').before('<span class="pl-5 pr-5 m-2 pb-1 pt-1 bg-primary resultsP" id="results">Ping</span>')
             console.log('ping')
         }else if((num % 5 === 0) && (num % 3 !== 0)){
             console.log('pong')
@@ -18,4 +20,13 @@ $(document).ready(function(){
     for(var index = 1; index <= input; index++){
         playPingPong(index)
     }
+
+    $('.playUser').click(function(){
+        $('#userInputArea').show()
+        // var input = 
+
+    });
+
+
 });
+
