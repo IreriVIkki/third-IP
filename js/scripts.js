c6f99eb11d0212dfe94c0f9595d1aa6a65d9fc6b
IreriVikki <wambsviki@gmail.com>
2018-07-13 17:42:46 +0300

    edits on the user logic to support total results display

--- js/scripts.js
+++ js/scripts.js
@@ -74,44 +74,9 @@ $(document).ready(function(){
             alert('Please Enter A Valid Number!!!!')
         }
     });
+
+
+    $('#playAgain').click(function () {
+        window.location.reload(true);
+    })
 });
-
-// $(document).ready(function () {
-
-//     //  business logic
-
-//     var input = 34
-
-//     function playPingPong(num) {
-//         if ((num % 3 === 0) && (num % 5 !== 0)) {
-//             $('#results').before('<div class="m-3"><span class="pl-5 pr-5 pb-1 pt-1 bg-primary resultsP">Ping</span></div>')
-//             console.log('ping')
-//         } else if ((num % 5 === 0) && (num % 3 !== 0)) {
-//             $('#results').before('<div class="m-3"><span class="pl-5 pr-5 pb-1 pt-1 bg-success resultsP">Pong</span></div>')
-//             console.log('pong')
-//         } else if (num % 15 === 0) {
-//             $('#results').before('<div class="m-3"><span class="pl-5 pr-5 pb-1 pt-1 bg-warning resultsP">Ping-Pong</span></div>')
-//             console.log('ping-pong')
-//         } else {
-//             $('#results').before('<div class="m-3"><span class="pl-5 pr-5 pb-1 pt-1 bg-secondary resultsP num"></span></div>')
-//             $('.num').text(num)
-//             console.log(num)
-//         }
-//     }
-
-
-
-//     for (var index = 1; index <= input; index++) {
-//         playPingPong(index)
-//     }
-
-//     $('.playUser').click(function () {
-//         $('#userInputArea').show()
-//         // var input = 
-
-//     });
-
-
-// });
-
-
