72292c6716285c578d84d5bca99f5bf419c25cb9
IreriVikki <wambsviki@gmail.com>
2018-07-13 12:32:35 +0300

    adding spans for pong

--- js/scripts.js
+++ js/scripts.js
@@ -6,9 +6,10 @@ $(document).ready(function(){
 
     function playPingPong(num){
         if((num % 3 === 0) && (num % 5 !== 0)){
-            $('#results').before('<span class="pl-5 pr-5 m-2 pb-1 pt-1 bg-primary resultsP" id="results">Ping</span>')
+            $('#results').before('<div class="m-3"><span class="pl-5 pr-5 pb-1 pt-1 bg-primary resultsP" id="results">Ping</span></div>')
             console.log('ping')
         }else if((num % 5 === 0) && (num % 3 !== 0)){
+            
             console.log('pong')
         }else if(num % 15 === 0){
             console.log('ping-pong')
